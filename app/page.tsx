cbc433304eb51560c1318c56466baff3cc6ba88c
peixotojeff <jefferson.-peixoto@hotmail.com>
2026-01-14 17:59:03 -0300

    Adicionei export const dynamic = 'force-dynamic' em page.tsx e page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 // src/app/page.tsx
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/contexts/AuthProvider';
